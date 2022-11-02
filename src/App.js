b2f3eb38bb1e2dcaf4c4d5f045ba0f5e6f876843
Sunghyun627 <kksscc4@gmail.com>
2022-11-02 13:24:24 +0900

    feat: controlBoard markup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import GameTitle from './components/GameTitle.js';
 import ScoreBoard from './components/ScoreBoard.js';
 import Board from './components/Board.js';
+import ControlBoard from './components/ControlBoard.js';
+
 export default class App {
   constructor(container) {
     this.container = container;
@@ -13,6 +15,7 @@ export default class App {
     new GameTitle(pageContainer);
     new ScoreBoard(pageContainer);
     new Board(pageContainer);
+    new ControlBoard(pageContainer);
     container.appendChild(pageContainer);
   }
 }
