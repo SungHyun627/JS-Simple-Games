b21447138e2734e82a625f02b43e1660364bf3b8
Sunghyun627 <kksscc4@gmail.com>
2022-11-02 12:50:48 +0900

    feat: 게임시작 전 초기 snake 위치와 임시의 초기 apple 위치 구현

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,15 @@
 import { BOARD_ROW_LENGTH } from '../constants/constants.js';
 
+const initialState = {
+  applePos: { x: 3, y: 10 },
+  snakeQueue: [
+    { x: 9, y: 6 },
+    { x: 9, y: 5 },
+    { x: 9, y: 4 },
+  ],
+  moveDir: 0,
+};
+
 export default class Board {
   constructor(container) {
     this.container = container;
@@ -27,6 +37,23 @@ export default class Board {
         cell.classList.add('cell-odd');
       }
 
+      initialState.snakeQueue.forEach((snakePos, snakeIdx) => {
+        const { x, y } = snakePos;
+        if (x === rowIdx && y === colIdx) {
+          cell.classList.add('snake__cell');
+          if (snakeIdx === 0) {
+            cell.classList.add('snake__head');
+          }
+        }
+      });
+
+      if (rowIdx === initialState.applePos.x && colIdx === initialState.applePos.y) {
+        cell.classList.add('apple__cell');
+        const apple = document.createElement('img');
+        apple.setAttribute('src', './src/assets/apple.svg');
+        apple.classList.add('apple-icon');
+        cell.appendChild(apple);
+      }
       boardContainer.appendChild(cell);
     });
     container.appendChild(boardContainer);
