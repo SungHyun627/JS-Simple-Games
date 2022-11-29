48811ec6461f5def08946abb94497441cd36344f
Sunghyun627 <kksscc4@gmail.com>
2022-11-29 14:44:44 +0900

    feat: snake가 벽과 충돌할 때 움직임이 종료되는 기능

--- src/App.js
+++ src/App.js
@@ -3,45 +3,23 @@ import ScoreBoard from './components/ScoreBoard.js';
 import Board from './components/Board.js';
 import ControlBoard from './components/ControlBoard.js';
 import Modal from './components/Modal.js';
-import { GAME_STATE } from './constants/constants.js';
-
-const initialState = {
-  gameState: GAME_STATE.BEFORE_START,
-};
 
 export default class App {
   constructor(container) {
     this.container = container;
-    this.state = { ...initialState };
-    this.render(container);
+    this.render();
   }
 
-  handleGameState(command) {
-    if (this.state.gameState === GAME_STATE.BEFORE_START && command === GAME_STATE.PLAYING)
-      this.setState({ gameState: GAME_STATE.PLAYING });
-    if (this.state.gameState === GAME_STATE.PLAYING && command === GAME_STATE.GAME_OVER)
-      this.setState({ gameState: GAME_STATE.GAME_OVER });
-  }
-
-  setState(newState) {
-    this.state = { ...this.state, ...newState };
-    console.log(this.state);
-  }
-
-  render(container) {
+  render() {
     const pageContainer = document.createElement('div');
     pageContainer.classList.add('page__container');
     new GameTitle(pageContainer);
     new ScoreBoard(pageContainer);
     new Board({
       container: pageContainer,
-      props: {
-        getGameState: () => this.state.gameState,
-        gameStateHandler: (command) => this.handleGameState(command),
-      },
     });
     new ControlBoard(pageContainer);
     new Modal(pageContainer);
-    container.appendChild(pageContainer);
+    this.container.appendChild(pageContainer);
   }
 }
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,12 +1,13 @@
 export const BOARD_ROW_LENGTH = 20;
-export const INTERVAL_TIME = 100;
+export const INTERVAL_TIME = 200;
 export const GAME_STATE = {
   BEFORE_START: 'Before Start',
   PLAYING: 'Playing',
   GAME_OVER: 'GameOver',
 };
-export const EVEMT_TYPES = {
+export const EVENT_TYPES = {
   CHANGE_DIRECTION: 'Change Direction',
   MOVE_FORWARD: 'Move Forward',
   GET_APPLE: 'Get Apple',
+  HIT_WALL: 'HIT_WALL',
 };
