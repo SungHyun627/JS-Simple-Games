99c6ecaced6833f9461dfc4b0f081ee9681075df
Sunghyun627 <kksscc4@gmail.com>
2023-01-13 12:05:14 +0900

    refactor: createApplePos 함수인자의 naming을 구체화

--- src/utils/createApplePosition.js
+++ src/utils/createApplePosition.js
@@ -3,9 +3,9 @@ import { comparePos } from './getPositions.js';
 
 const getRandom = () => Math.floor(Math.random() * BOARD_ROW_LENGTH);
 
-export const createApplePos = (snakeState) => {
+export const createApplePos = (snakeQueue) => {
   const applePos = { x: getRandom(), y: getRandom() };
-  if (snakeState.some((snakePos) => comparePos(snakePos, applePos)))
-    return createApplePos(snakeState);
+  if (snakeQueue.some((snakePos) => comparePos(snakePos, applePos)))
+    return createApplePos(snakeQueue);
   return applePos;
 };
